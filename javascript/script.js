0b1acc7a653f1e8e3bb5727ad425fd21f7930447
fernandoJesuss <fernando_ffernando@hotmail.com>
2025-05-03 19:35:54 -0300

    Finishing footer

--- javascript/script.js
+++ javascript/script.js
@@ -1,4 +1,9 @@
+const hamburger = document.getElementById('hamburger');
+    const menuList = document.getElementById('menu-list');
 
+    hamburger.addEventListener('click', () => {
+        menuList.classList.toggle('active'); // Alterna a classe 'active' no menu
+    });
 
 
 
